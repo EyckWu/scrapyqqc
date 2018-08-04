d5e5221dd372f2e8cab6013f0fddd899cac13682
WuGuiXin <EyckWu@gmail.com>
2018-08-04 09:01:59 +0800

    feat(BDTB):去掉网页中的标签

--- ch12/BDBK.py
+++ ch12/BDBK.py
@@ -33,11 +33,13 @@ class BDTB(object):
         pattern = re.compile(r'<h3 class="core_title_txt.*?>(.*?)</h3>',re.S)
         item = re.search(pattern, page)
         print(item.group(1).strip())
+        return item.group(1).strip()
 
     def getItemCount(self, page):
         pattern = re.compile(r'<li class="l_reply_num" style="margin-left:8px" >.*?class="red">(.*?)</span>', re.S)
         item = re.search(pattern, page)
         print(item.group(1).strip())
+        return item.group(1).strip()
 
     def getContent(self, page):
         pattern = re.compile(r'<div id="post_content.*?">(.*?)</div>', re.S)
@@ -55,16 +57,42 @@ class BDTB(object):
         if title is not None:
             self.file = open(title + '.txt', 'w+')
         else:
-            self.file = open(self.default + '.txt', 'w+')
+            self.file = open(self.defaultTitle + '.txt', 'w+')
 
     def writeData(self, contents):
         for item in contents:
-            print(item)
+            if self.floorTag == '1':
+                # 楼之间的分隔符
+                floorLine = "\n" + str(
+                    self.floor) + u"-----------------------------------------------------------------------------------------\n"
+                self.file.write(floorLine)
+            self.file.write(item)
+            self.floorTag += 1
+
+    def start(self):
+        indexPage = self.getPage(1)
+        pageNum = self.getItemCount(indexPage)
+        pageTitle = self.getTitle(indexPage)
+        self.setTitle(pageTitle)
+        if pageNum == None:
+            print ('请重试')
+            return
+        print('该帖子共有%d页帖子' % int(pageNum))
+        try:
+            for i in range(1, (int)(pageNum) + 1):
+                print ('正在写入第%d页数据' % i)
+                page = self.getPage(i)
+                contents = self.getContent(page)
+                self.writeData(contents)
+            print('数据写入完毕')
+        except IOError, e:
+            print e.message
 
 baseUrl = 'http://tieba.baidu.com/p/3138733512'
-bdtb = BDTB(baseUrl, 1, 'hhh')
-page = bdtb.getPage(1)
-bdtb.getTitle(page)
-bdtb.getItemCount(page)
-bdtb.getContent(page)
+bdtb = BDTB(baseUrl, 1, 1)
+bdtb.start()
+# page = bdtb.getPage(1)
+# bdtb.getTitle(page)
+# bdtb.getItemCount(page)
+# bdtb.getContent(page)
 
